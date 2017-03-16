086c069f66aedc29df35ca2a98a83fd53ccd0dfb
Patryktok <patryktok@gmail.com>
2017-03-16 19:37:05 +0100

    button v4

--- index.php
+++ index.php
@@ -66,9 +66,11 @@ $response = [
 
 ];
 
+$res = '{"recipient":{"id":"USER_ID"},"message":{"attachment":{"type":"template","payload":{"template_type":"button","text":"What do you want to do next?","buttons":[{"type":"web_url","url":"https://petersapparel.parseapp.com","title":"Show Website"},{"type":"postback","title":"Start Chatting","payload":"USER_DEFINED_PAYLOAD"}]}}}}';
+
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $res);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_exec($ch);
 curl_close($ch);
