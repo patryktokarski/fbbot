93f6b015fbc9a855b420f727bf4284a58b5bb9ff
Patryktok <patryktok@gmail.com>
2017-03-17 17:08:38 +0100

    words collection

---
+++ Words.php
@@ -0,0 +1,45 @@
+<?php
+
+class Words {
+
+    public $welcomeMessage = [
+        'czesc',
+        'cześć',
+        'witam',
+        'siema',
+        'elo',
+        'siemanko',
+        'witaj',
+        'dzień dobry'
+    ];
+
+    public $welcomeMessageResponse = [
+        'Witaj',
+        'Cześć',
+        'Dzień dobry',
+    ];
+
+    public $goodbyeMessage = [
+        'do widzenia',
+        'nara',
+        'do zobaczenia'
+    ];
+
+    public $goodbyeMessageResponse = [
+        'Do zobaczenia',
+        'Do widzenia'
+    ];
+
+    public $help = [
+        'help',
+        'pomoc',
+        'podpowiedź',
+        'opcje'
+    ];
+
+    public $regulations = [
+        'regulamin'
+    ];
+}
+
+?>
